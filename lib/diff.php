3e93714864660cc47062460ebb37b013fec61eb0
Ralf Becker <RalfBecker@outdoor-training.de>
2018-05-14 19:33:28 +0200

    you could use Api\Auth::randomstring(), but stock PHP tempnam is what you want

--- lib/diff.php
+++ lib/diff.php
@@ -6,10 +6,8 @@ function diff_compute($text1, $text2)
 {
 	global $TempDir, $DiffCmd, $ErrorCreatingTemp, $ErrorWritingTemp;
 
-	$num = trim($GLOBALS['egw_info']['user']['account_id']).'_'.randomstring(8); //strncmp(PHP_OS,'WIN',3) ? posix_getpid() : rand();
-	//error_log(__METHOD__.__LINE__.' RandomString:'.$num);
-	$temp1 = $TempDir . '/wiki_' . $num . '_1.txt';
-	$temp2 = $TempDir . '/wiki_' . $num . '_2.txt';
+	$temp1 = tempname($TempDir, 'wiki_');
+	$temp2 = tempname($TempDir, 'wiki_');
 
 	if(!($h1 = fopen($temp1, 'w')) || !($h2 = fopen($temp2, 'w')))
 		{ die($ErrorCreatingTemp); }
@@ -35,29 +33,3 @@ function diff_parse($text)
 
 	return parseText($text, $DiffEngine, '');
 }
-
-/**
- * return a random string of letters [0-9a-zA-Z] of size $size
- * Copied from Api\Auth
- * @param $size int-size of random string to return
- */
-function randomstring($size)
-{
-	$random_char = array(
-		'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',
-		'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-		'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L',
-		'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-	);
-
-	// use cryptographically secure random_int available in PHP 7+
-	$func = function_exists('random_int') ? 'random_int' : 'mt_rand';
-
-	$s = '';
-	for ($i=0; $i < $size; $i++)
-	{
-		$s .= $random_char[$func(0, count($random_char)-1)];
-	}
-	return $s;
-}
-?>
